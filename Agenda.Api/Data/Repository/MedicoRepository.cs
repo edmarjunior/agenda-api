19d7e12d08a0194c7f943873ed40e29fb2f48be1
Edmar Costa <edmar@smn.com.br>
2019-11-12 09:25:10 -0300

    alteração no repository de medico para sobreescrever o método get do repository generico de pessoa

--- Agenda.Api/Data/Repository/MedicoRepository.cs
+++ Agenda.Api/Data/Repository/MedicoRepository.cs
@@ -1,14 +1,24 @@
 ﻿using Agenda.Api.Data.Context;
 using Agenda.Api.Models;
 using Agenda.Api.Services.Medicos;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Agenda.Api.Data.Repository
 {
     public class MedicoRepository : PessoaRepository<Medico>, IMedicoRepository
     {
-        public MedicoRepository(AgendaContext context) :base(context)
+        public MedicoRepository(AgendaContext context) : base(context)
         {
 
+        }
+
+        public new Medico Get(int id)
+        {
+            return _context.Medicos.AsNoTracking()
+                .Include(x => x.Endereco)
+                .Include(x => x.Telefone).ThenInclude(x => x.Tipo)
+                .Where(x => x.Id == id).FirstOrDefault();
         }
     }
 }
